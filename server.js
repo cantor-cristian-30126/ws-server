60998bbfdd8a26e467f3e6b0cb86ba7f455bdc7e
cantor-cristian-30126 <cantorcristian16@gmail.com>
2026-04-19 13:21:22 +0300

    clean start

---
+++ server.js
@@ -0,0 +1,48 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+let cameraClient = null;
+let viewers = [];
+
+console.log("Server pornit pe port 8080");
+
+wss.on("connection", (ws) => {
+    console.log("Client conectat");
+
+    ws.on("message", (message) => {
+        try {
+            const data = JSON.parse(message);
+
+            if (data.type === "camera") {
+                cameraClient = ws;
+                console.log("Camera conectată");
+            }
+
+            if (data.type === "viewer") {
+                viewers.push(ws);
+                console.log("Viewer conectat");
+            }
+
+        } catch (e) {
+            // imagine de la ESP
+            if (ws === cameraClient) {
+                viewers.forEach((viewer) => {
+                    if (viewer.readyState === WebSocket.OPEN) {
+                        viewer.send(message);
+                    }
+                });
+            }
+        }
+    });
+
+    ws.on("close", () => {
+        console.log("Client deconectat");
+
+        if (ws === cameraClient) {
+            cameraClient = null;
+        }
+
+        viewers = viewers.filter(v => v !== ws);
+    });
+});
